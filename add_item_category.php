8e1cfdbd831fd0cc67efae88e813b181de21312e
AlvinSadeque <alvinsadeque@gmail.com>
2023-11-22 22:46:53 -0700

    updated add item and add category

--- add_item_category.php
+++ add_item_category.php
@@ -4,12 +4,16 @@
         <h1>Add Item Category</h1>
     </head>
 
-    <form>
+    <a href="main_page.php">
+        <button><img src="images/back.png" width="10" length="10"></button>
+    </a><br>
+
+    <form action="main_page.php">
         <label for="catname">Item Category Name:</label><br>
         <input type="text" id="catname" name="catname"><br>
 
         <br>
-        <input type="submit" value="Submit" >
+        <input type="submit" value="Add" >
     </form>
 
 </html>
