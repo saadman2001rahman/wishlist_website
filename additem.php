8f6c3120e01277b865718c6998ff3168f3d96d39
AlvinSadeque <alvinsadeque@gmail.com>
2023-11-22 20:44:31 -0700

    Create additem.php

---
+++ additem.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<head>
+    <h1>Add Item</h1>
+</head>
+
+<form>
+    <label for="itemname">Item name:</label><br>
+    <label type="text" id="itemname" name="itemname"><br>
+    <label for="itemprice">Price:</label><br>
+    <label type="text" id="itemprice" name="itemprice"><br>
+</form>
+
+</html>
