5ad5cfe8fc7ad20497abd435c452be6633ee7e6d
AlvinSadeque <alvinsadeque@gmail.com>
2023-12-07 21:11:27 -0700

    updated add item

--- add_item.php
+++ add_item.php
@@ -4,19 +4,19 @@
         <h1>Add Item</h1>
     </head>
 
-    <a href="sql_add_item.php">
+    <a href="main_page.php">
         <button><img src="images/back.png" width="10" length="10"></button>
     </a>
 
-    <form action="main_page.php" method="post">
+    <form action="sql_add_item.php" method="post">
         <label for="iname">Item name:</label>
         <input type="text" id="iname" name="iname"><br>
         <br>
         <label for="price">Price:</label>
         <input type="number" id="price" name="price" step="0.01" min="0.00"><br>
         <br>
-        <label for="category">Item category:</label>
-        <select name="category" id="category">
+        <label for="icat">Item category:</label>
+        <select name="icat" id="icat">
             <?php
             //https://www.w3schools.com/howto/howto_js_cascading_dropdown.as
             //TODO: Connect categories from main_page categories
@@ -29,8 +29,8 @@
             ?>
         </select><br>
         <br>
-        <label for="itemdue">Item due date: </label>
-        <input type="date" id="itemdue" name="itemdue" max="2025-12-31"><br>
+        <label for="ddate">Item due date: </label>
+        <input type="date" id="ddate" name="ddate" max="2025-12-31"><br>
         <br>
         <label for="description">Description:</label>
         <input type="text" id="description" name="description"><br>
--- sql_add_item.php
+++ sql_add_item.php
@@ -1,12 +1,17 @@
 <?php
 
 $iname = $_POST["item_name"];
-$ddate = $_POST["due_date"];
-$link = $_POST["link"];
-$description = $_POST["description"];
-$icat = $_POST["item_category"];
-$domain = $_POST["website_domain"];
 $price = $_POST["price"];
+// $icat = $_POST["item_category"];
+$ddate = $_POST["due_date"];
+$description = $_POST["description"];
+$link = $_POST["link"];
+
+$itemid = rand(0,9999);
+
+// $domain = $_POST["website_domain"];
+// $wishid = $_POST["wishlist_id"];
+// $baskid = $_POST["basket_id"];
 
 // echo $iname; ??
 
@@ -18,8 +23,9 @@ if(!$con) {
     echo "Failed to connect: ". mysqli_connect_error();
 }
 
+// May need to change item category; website domain; wishlist id; basket id
 $sql = "INSERT INTO item (Item_number, Name, Due_date, Link, Description, Item_category, Website_domain, Wishlist_id, Basket_id, Price)
-    VALUES (rand(), '$iname', '$ddate', '$link', '$description', '$icat', '$domain', rand(), rand(), '$price')";
+        VALUES ($itemid, '$iname', '$ddate', '$link', '$description', NULL, NULL, '10001', NULL, '$price')";
 
 if(!mysqli_query($con,$sql)) {
     die('Error: '. mysqli_error($con));
