0f23d31ecc0ef0c359d6726c60b8dba77926edd9
ManpreetM1 <87155707+ManpreetM1@users.noreply.github.com>
2023-12-07 21:34:48 -0700

    Added stylesheets

--- signup.php
+++ signup.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html> 
 <html>
  <head>
+ <link rel="stylesheet" href="style.css">
  <h1> Register a new account </h1>
  </head>
 
--- sql_add_user.php
+++ sql_add_user.php
@@ -1,5 +1,7 @@
 <?php
 
+$canInsert = 0;
+
 $dname = $_POST["display_name"];
 $pass = $_POST["password"];
 $email = $_POST["email"];
@@ -16,7 +18,23 @@ if (!$con)
   }
 
   $form_filled = true;
-
+  
+  // Ensures random number for user_id isn't in use in sql database already 
+  $rand = rand(0,9999999);
+  while($canInsert == 0) {
+    $rand = rand(0,9999999);
+    $result = mysqli_query($con," SELECT User_id FROM master_user WHERE User_id = '$rand'");
+    if($result !== false) {
+      if(mysqli_num_rows($result) == 0) {
+        $canInsert = 1;
+      } else {
+        $rand = rand(0,9999999);
+      }
+    } else {
+      echo 'Error in query: ' . mysqli_error($con);
+      die();
+    }
+  }
   /*foreach ( $_POST as $key) {
     if(!$isset($key)) {
         $form_filled = false;
@@ -25,7 +43,7 @@ if (!$con)
 
  // if($form_filled) {
     $sql = "INSERT INTO master_user (User_id,Email_address,Display_name,User_address,Phone_number,User_password) 
-                VALUES (rand(),'$email','$dname','$address','$phone','$pass')";
+                VALUES ($rand,'$email','$dname','$address','$phone','$pass')";
     
     
     if (!mysqli_query($con,$sql))
