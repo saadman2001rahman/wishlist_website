b7f0a925a2207043a78c3b2ffbd7cbe1465a24e2
saadman2001rahman <saadman2001rahman@gmail.com>
2023-12-11 02:29:19 -0700

    can add item to basket now. Comfirm resubmission required. need fix

--- sql_add_to_basket.php
+++ sql_add_to_basket.php
@@ -1,4 +1,7 @@
 <?php
+session_start();
+$owner_id = $_SESSION["Owner_id"];
+
 $item_id = $_POST['item_number'];
 $wishlist_id = $_POST['wishlist_id'];
 
@@ -12,7 +15,19 @@ if (!$con) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-$sql = "UPDATE item SET basket_id='10' WHERE item_number = '$item_id'";
+$sql1 = "SELECT Basket_id FROM Basket Where User_id = '$owner_id'";
+$result1 = mysqli_query($con, $sql1);
+if (!$result1) {
+    die('Error: ' . mysqli_error($con));
+}
+
+$all_categories = mysqli_fetch_all($result1, MYSQLI_ASSOC);
+foreach ($all_categories as $row) {
+    $basket_id = $row['Basket_id'];
+}
+
+
+$sql = "UPDATE item SET basket_id='$basket_id' WHERE item_number = '$item_id'";
 
 if (!mysqli_query($con, $sql)) {
     die('Error: ' . mysqli_error($con));
